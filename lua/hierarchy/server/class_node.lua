59cd1bb275801f564e1c73ada6550cc377b5adf9
Massimiliano della Rovere <massimiliano.dellarovere@gmail.com>
2026-02-11 10:53:30 +0100

    Turned RELOAD into require

--- lua/hierarchy/server/class_node.lua
+++ lua/hierarchy/server/class_node.lua
@@ -1,5 +1,5 @@
 --[[ ───────────────────────── imports / aliases ────────────────────────── ]]--
-local utils = RELOAD('hierarchy.server.utils')
+local utils = require('hierarchy.server.utils')
 
 
 --[[ ───────────────────────────── Class Node ───────────────────────────── ]]--
